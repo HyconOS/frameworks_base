b8baa2532ca9c26817cbba8d284c7e947dba5e6d
Adnan Begovic <adnan@cyngn.com>
2015-12-11 16:16:02 -0800

    SystemUI: QS: Add AmbientDisplay tile
    Stallix: Adapt for 10
    
    Author: Adnan Begovic <adnan@cyngn.com>
    Date:   Fri Dec 11 16:16:02 2015 -0800
    
        SystemUI: Readd AmbientDisplayTile.
    
        Change-Id: Ie7b93ba0f87d505206b5d60f193c2234d83d7eac
    
    Author: Michael Bestas <mkbestas@lineageos.org>
    Date:   Sun Sep 17 22:51:15 2017 +0300
    
        SystemUI: Hide ambient display tile if device does not support it
    
        * Add a check similar to the Settings app
    
        Change-Id: I603cd28b8dd1fadff9ffcfb6953c7b72514b5857
    
    Change-Id: Iea339685f523016876f059052e66e97fd5170a81
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -28,6 +28,7 @@ import com.android.systemui.plugins.qs.QSTileView;
 import com.android.systemui.qs.QSHost;
 import com.android.systemui.qs.external.CustomTile;
 import com.android.systemui.qs.tiles.AirplaneModeTile;
+import com.android.systemui.qs.tiles.AmbientDisplayTile;
 import com.android.systemui.qs.tiles.AODTile;
 import com.android.systemui.qs.tiles.BatterySaverTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
@@ -95,6 +96,7 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<GamingModeTile> mGamingModeTileProvider;
     private final Provider<HeadsUpTile> mHeadsUpTileProvider;
     private final Provider<AODTile> mAODTileProvider;
+    private final Provider<AmbientDisplayTile> mAmbientDisplayTileProvider;
 
     private final Lazy<QSHost> mQsHostLazy;
 
@@ -126,7 +128,8 @@ public class QSFactoryImpl implements QSFactory {
             Provider<SyncTile> syncTileProvider,
             Provider<GamingModeTile> gamingModeTileProvider,
             Provider<HeadsUpTile> headsUpTileProvider,
-            Provider<AODTile> aodTileProvider) {
+            Provider<AODTile> aodTileProvider,
+            Provider<AmbientDisplayTile> ambientDisplayTileProvider) {
         mQsHostLazy = qsHostLazy;
         mWifiTileProvider = wifiTileProvider;
         mBluetoothTileProvider = bluetoothTileProvider;
@@ -157,6 +160,7 @@ public class QSFactoryImpl implements QSFactory {
         mGamingModeTileProvider = gamingModeTileProvider;
         mHeadsUpTileProvider = headsUpTileProvider;
         mAODTileProvider = aodTileProvider;
+        mAmbientDisplayTileProvider = ambientDisplayTileProvider;
     }
 
     public QSTile createTile(String tileSpec) {
@@ -223,6 +227,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mHeadsUpTileProvider.get();
             case "aod":
                 return mAODTileProvider.get();
+            case "ambient_display":
+                return mAmbientDisplayTileProvider.get();
         }
 
         // Custom tiles

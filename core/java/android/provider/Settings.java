7f3be7493964ad44e63ec8879644bc80b9f994a7
maxwen <max.weninger@gmail.com>
2017-09-02 14:27:50 +0800

    base: Add Wakelock Blocker [1/2]
    ported by faust93
    
    neobuddy89: Moved to global settings.
    
    Change-Id: I93fc64127c63a2d7986dff4f9c2a9941eaa56102
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14494,6 +14494,18 @@ public final class Settings {
         public static final String SHOW_CPU_OVERLAY = "show_cpu_overlay";
 
         /**
+         * Whether to enable Wakelock blocker
+         * @hide
+         */
+        public static final String WAKELOCK_BLOCKING_ENABLED = "wakelock_blocking_enabled";
+
+        /**
+         * List of blocked wakelocks
+         * @hide
+         */
+        public static final String WAKELOCK_BLOCKING_LIST = "wakelock_blocking_list";
+
+        /**
          * Global settings that shouldn't be persisted.
          *
          * @hide

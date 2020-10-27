0690267e957ca6280af4a082f7f0bf658f3e6c9b
ezio84 <brabus84@gmail.com>
2020-10-27 20:33:55 -0300

    Allow longpress power button to toggle torch [1/2]
    * Adapt to AOSPA by @brituj
    * Adapt to Android 11 by @mickaelmendes50
    
    * Thanks beanstown106 and lineage guys for the initial longpress action
    bringup in PhoneWindowManager
    
    * Allow torch action also on ambient display
    
    [Sam Mortimer] fw/b torch: Let long press power turn torch off when screen is on.
    
    * When the torch is on, any subsequent long press power is almost certainly
      intended to turn the torch off (regardless of screen state).  Therefore,
      always allow long press power to toggle torch if the torch is on.
    
    * Tested: long press power toggles torch on/off with screen off.
              long press power toggles torch off with screen on and torch on.
              long press power brings up global actions menu with screen on and torch off.
    
    Change-Id: I12da044f86c7b625872607529cf8524615cf576b
    Co-authored-by: Sam Mortimer <sam@mortimer.me.uk>
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: AshutoshSundresh <ashutoshsundresh@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -170,6 +170,7 @@ public class SecureSettings {
         Settings.Secure.ADAPTIVE_CONNECTIVITY_ENABLED,
         Settings.Secure.ASSIST_HANDLES_LEARNING_TIME_ELAPSED_MILLIS,
         Settings.Secure.ASSIST_HANDLES_LEARNING_EVENT_COUNT,
-        Settings.Secure.VOLUME_LINK_NOTIFICATION
+        Settings.Secure.VOLUME_LINK_NOTIFICATION,
+        Settings.Secure.TORCH_POWER_BUTTON_GESTURE
     };
 }

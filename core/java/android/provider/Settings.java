20c1bb782906fb4623d45f7d921200fbee6413d1
TikkiTikki <radstylemusic@gmail.com>
2021-02-03 21:43:34 -0300

    base: Replace media notification background with artwork [1/2] Change-Id: I52e511955e2a39e73645e6a1a64a0bd8493ac18b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5738,6 +5738,12 @@ public final class Settings {
         public static final String HEADS_UP_BLACKLIST_VALUES = "heads_up_blacklist_values";
 
         /**
+         * Enable and disable Artwork on background media notification
+         * @hide
+         */
+        public static final String ARTWORK_MEDIA_BACKGROUND = "artwork_media_background";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *

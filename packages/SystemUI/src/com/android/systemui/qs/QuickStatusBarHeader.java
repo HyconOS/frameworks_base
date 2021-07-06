0724368f6b8a5e1af6186105470f6aae72a352fc
anuragbhaumik <bhaumik463@gmail.com>
2021-07-06 13:18:33 +0530

    Revert "QSHeaderDataUsage: add icon and improve view"
    This reverts commit d27aeafd8f567233aba3a23d6795fb7e4768ec7f.

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -175,10 +175,6 @@ public class QuickStatusBarHeader extends RelativeLayout implements
 
     private PrivacyItemController mPrivacyItemController;
     private final UiEventLogger mUiEventLogger;
-
-    // Data Usage
-    private View mDataUsageLayout;
-    private ImageView mDataUsageImage;
     private DataUsageView mDataUsageView;
 
     // Used for RingerModeTracker
@@ -283,8 +279,6 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         mIconManager = new TintedIconManager(iconContainer, mCommandQueue);
 
         mDataUsageView = findViewById(R.id.data_sim_usage);
-        mDataUsageLayout = findViewById(R.id.daily_data_usage_layout);
-        mDataUsageImage = findViewById(R.id.daily_data_usage_icon);
 
         // Views corresponding to the header info section (e.g. ringer and next alarm).
         mHeaderTextContainerView = findViewById(R.id.header_text_container);
@@ -534,15 +528,10 @@ public class QuickStatusBarHeader extends RelativeLayout implements
     }
 
     private void updateDataUsageView() {
-        if (mDataUsageView.isDataUsageEnabled()!= 0) {
-            mDataUsageLayout.setVisibility(View.VISIBLE);
-            mDataUsageImage.setVisibility(View.VISIBLE);
+        if (mDataUsageView.isDataUsageEnabled()!= 0)
             mDataUsageView.setVisibility(View.VISIBLE);
-        } else {
+        else
             mDataUsageView.setVisibility(View.GONE);
-            mDataUsageImage.setVisibility(View.GONE);
-            mDataUsageLayout.setVisibility(View.GONE);
-        }
     }
 
     public void setExpanded(boolean expanded) {

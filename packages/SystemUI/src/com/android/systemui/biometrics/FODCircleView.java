6a8ca8d77f7751118f0b718ca3e5f6c136f5a7a5
Shubham <skpawar1305@gmail.com>
2021-08-26 06:47:05 +0000

    FODCircleView: call dimlayer during onPress while screen off FOD is turned on
    for guacamoleb, current implementation doesn't allow the screen to wake up
    this can be avoided by calling dimlayer during onshow while screen off FOD is on
    if it's hardcoded through the device tree, it will make FOD slower and flicker even when screen off FOD is turned off
    *control this through an overlay

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -113,6 +113,7 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
     private boolean mFodGestureEnable;
     private boolean mPressPending;
     private boolean mScreenTurnedOn;
+    private boolean mDimlayerSOF;
 
     private PowerManager mPowerManager;
     private PowerManager.WakeLock mWakeLock;
@@ -486,6 +487,8 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
             com.android.internal.R.bool.config_supportsScreenOffInDisplayFingerprint)){
             mFodGestureSettingsObserver = new FodGestureSettingsObserver(context, mHandler);
             mFodGestureSettingsObserver.registerListener();
+            mDimlayerSOF = context.getResources().getBoolean(
+                    com.android.internal.R.bool.config_disableCallingDimlayerInOnShowWithScreenOffFOD);
         }
 
         updateCutoutFlags();
@@ -592,6 +595,9 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
         if (mFading) return;
         IFingerprintInscreen daemon = getFingerprintInScreenDaemon();
         try {
+            if (mDimlayerSOF && mFodGestureEnable) {
+                daemon.onShowFODView();
+            }
             daemon.onPress();
         } catch (RemoteException e) {
             // do nothing
@@ -602,6 +608,9 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
         IFingerprintInscreen daemon = getFingerprintInScreenDaemon();
         try {
             daemon.onRelease();
+            if (mDimlayerSOF && mFodGestureEnable) {
+                daemon.onHideFODView();
+            }
         } catch (RemoteException e) {
             // do nothing
         }
@@ -610,7 +619,11 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
     public void dispatchShow() {
         IFingerprintInscreen daemon = getFingerprintInScreenDaemon();
         try {
-            daemon.onShowFODView();
+            if (!mDimlayerSOF) {
+                daemon.onShowFODView();
+            } else if (!mFodGestureEnable) {
+                daemon.onShowFODView();
+            }
         } catch (RemoteException e) {
             // do nothing
         }
